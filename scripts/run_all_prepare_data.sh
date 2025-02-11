b88305e484fa2222ab10c2b954512dd94f30e370
bowen-upenn <bwjiang@seas.upenn.edu>
2025-02-10 19:55:31 -0500

    add new script to run data generation

---
+++ scripts/run_all_prepare_data.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Arrays for the parameters
+idx_personas=$(seq 1 1)
+
+# Iterate over each combination of parameters
+for idx_persona in $idx_personas; do
+        # Construct the command
+        command="python prepare_data.py --model gpt-4o \
+                 --topics therapy legalConsultation datingConsultation foodRecommendation onlineShopping studyConsultation travelPlanning bookRecommendation \
+                          movieRecommendation songRecommendation sportsRecommendation homeDecoration financialConsultation healthConsultation \
+                          writing, email, coding \
+                 --n_persona ${idx_persona} --n_samples 1 --s_persona 0 --s_samples 0 --clean"
+
+        # Print the command for debugging/logging purposes
+        echo "$command"
+
+        # Execute the command
+        eval "$command"
+done
