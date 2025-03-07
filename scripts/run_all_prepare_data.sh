8118c7e70db4986c1a13550cebf0db4f3cd26687
JeffreyCh0 <cym940514@gmail.com>
2025-03-07 15:32:50 -0500

    merged with changes

--- scripts/run_all_prepare_data.sh
+++ scripts/run_all_prepare_data.sh
@@ -5,15 +5,12 @@
 # legalConsultation medicalConsultation movieRecommendation musicRecommendation onlineShopping sportsRecommendation \
 # studyConsultation therapy travelPlanning writing \
 
-<<<<<<< Updated upstream
 ## Lauren
 start_persona_id=1
 end_persona_id=4  # non-inclusive
-=======
 # Lauren
 # start_persona_id=3
 # end_persona_id=4  # non-inclusive
->>>>>>> Stashed changes
 
 ## Zoey
 #start_persona_id=4
@@ -32,16 +29,13 @@ end_persona_id=16
 #end_persona_id=20
 
 # Construct the command
-<<<<<<< Updated upstream
 command="python prepare_data.py --model gpt-4o \
         --topics bookRecommendation coding datingConsultation email familyRelations financialConsultation foodRecommendation homeDecoration \
                  legalConsultation medicalConsultation movieRecommendation musicRecommendation onlineShopping sportsRecommendation \
                  studyConsultation therapy travelPlanning writing \ \
         --n_persona ${end_persona_id} --n_samples 1 --s_persona ${start_persona_id} --s_samples 0 --output_dir data/output/ "
-=======
 command="python prepare_data.py --model gpt-4o --topics onlineShopping \
 --n_persona ${end_persona_id} --n_samples 1 --s_persona ${start_persona_id} --s_samples 0 --output_dir data/output/ "
->>>>>>> Stashed changes
 
 # Print the command for debugging/logging purposes
 echo "$command"
