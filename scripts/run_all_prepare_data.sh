ea2e8177f439455f0c1cba263dd879fa6a44533b
JeffreyCh0 <cym940514@gmail.com>
2025-03-08 11:39:29 -0500

    reran 12

--- scripts/run_all_prepare_data.sh
+++ scripts/run_all_prepare_data.sh
@@ -6,8 +6,8 @@
 # studyConsultation therapy travelPlanning writing \
 
 ## Lauren
-start_persona_id=1
-end_persona_id=4  # non-inclusive
+# start_persona_id=1
+# end_persona_id=4  # non-inclusive
 # Lauren
 # start_persona_id=3
 # end_persona_id=4  # non-inclusive
@@ -21,8 +21,8 @@ end_persona_id=4  # non-inclusive
 #end_persona_id=12
 
 ## Jeff
-start_persona_id=15
-end_persona_id=16
+start_persona_id=12
+end_persona_id=13
 #
 ## Brian
 #start_persona_id=16
@@ -30,11 +30,9 @@ end_persona_id=16
 
 # Construct the command
 command="python prepare_data.py --model gpt-4o \
-        --topics bookRecommendation coding datingConsultation email familyRelations financialConsultation foodRecommendation homeDecoration \
-                 legalConsultation medicalConsultation movieRecommendation musicRecommendation onlineShopping sportsRecommendation \
-                 studyConsultation therapy travelPlanning writing \ \
-        --n_persona ${end_persona_id} --n_samples 1 --s_persona ${start_persona_id} --s_samples 0 --output_dir data/output/ "
-command="python prepare_data.py --model gpt-4o --topics onlineShopping \
+--topics bookRecommendation coding datingConsultation email familyRelations financialConsultation foodRecommendation homeDecoration \
+legalConsultation medicalConsultation movieRecommendation musicRecommendation onlineShopping sportsRecommendation \
+studyConsultation therapy travelPlanning writing \
 --n_persona ${end_persona_id} --n_samples 1 --s_persona ${start_persona_id} --s_samples 0 --output_dir data/output/ "
 
 # Print the command for debugging/logging purposes
