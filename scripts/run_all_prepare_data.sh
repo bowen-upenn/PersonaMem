adc0e7f6df52ae846cdc0740f09633bdc21b2c40
bowen-upenn <bwjiang@seas.upenn.edu>
2025-03-09 00:16:07 -0500

    add conversation data of persona ids 0-3 and 8-11

--- scripts/run_all_prepare_data.sh
+++ scripts/run_all_prepare_data.sh
@@ -5,31 +5,29 @@
 #legalConsultation medicalConsultation movieRecommendation musicRecommendation onlineShopping sportsRecommendation \
 #studyConsultation therapy travelPlanning writing \
 
-## Lauren
-start_persona_id=1
+# Lauren
+start_persona_id=3
 end_persona_id=4  # non-inclusive
 
 ## Zoey
 #start_persona_id=4
 #end_persona_id=8
-#
-# Yuan
-#start_persona_id=8
-#end_persona_id=12
+
+## Yuan
+#start_persona_id=10
+#end_persona_id=11
 
 ## Jeff
 #start_persona_id=12
 #end_persona_id=16
-#
+
 ## Brian
 #start_persona_id=16
 #end_persona_id=20
 
 # Construct the command
 command="python prepare_data.py --model gpt-4o \
-        --topics bookRecommendation coding datingConsultation email familyRelations financialConsultation foodRecommendation homeDecoration \
-                 legalConsultation medicalConsultation movieRecommendation musicRecommendation onlineShopping sportsRecommendation \
-                 studyConsultation therapy travelPlanning writing \ \
+        --topics studyConsultation \
         --n_persona ${end_persona_id} --n_samples 1 --s_persona ${start_persona_id} --s_samples 0 --output_dir data/output/ "
 
 # Print the command for debugging/logging purposes
