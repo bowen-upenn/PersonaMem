1166a57b5d56481221be07d2a27fbd632bd70bbc
bowen-upenn <bwjiang@seas.upenn.edu>
2025-03-10 17:13:29 -0400

    add warning if no valid Q&As are generated

--- scripts/run_all_prepare_data.sh
+++ scripts/run_all_prepare_data.sh
@@ -6,8 +6,8 @@
 #studyConsultation therapy travelPlanning writing \
 
 # Lauren
-start_persona_id=3
-end_persona_id=4  # non-inclusive
+start_persona_id=0
+end_persona_id=1  # non-inclusive
 
 ## Zoey
 #start_persona_id=4
@@ -27,7 +27,7 @@ end_persona_id=4  # non-inclusive
 
 # Construct the command
 command="python prepare_data.py --model gpt-4o \
-        --topics studyConsultation \
+        --topics datingConsultation \
         --n_persona ${end_persona_id} --n_samples 1 --s_persona ${start_persona_id} --s_samples 0 --output_dir data/output/ "
 
 # Print the command for debugging/logging purposes
