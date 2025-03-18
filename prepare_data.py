0f345df758b9fa5a37f5037d1c6325da555bbb9b
zhuoqunhao <zhuoqunh@sas.upenn.edu>
2025-03-17 22:15:53 -0700

    tryied to fix error for persona6 medical

--- prepare_data.py
+++ prepare_data.py
@@ -414,7 +414,7 @@ if __name__ == "__main__":
     torch.manual_seed(0)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     world_size = torch.cuda.device_count()
-    assert world_size == 1
+    #assert world_size == 1
     print('device', device)
     print('torch.distributed.is_available', torch.distributed.is_available())
     print('Using %d GPUs' % (torch.cuda.device_count()))
--- scripts/run_all_prepare_data.sh
+++ scripts/run_all_prepare_data.sh
@@ -27,9 +27,7 @@ end_persona_id=5
 
 # Construct the command
 command="python prepare_data.py --model gpt-4o \
-         --topics bookRecommendation coding datingConsultation email familyRelations financialConsultation foodRecommendation homeDecoration \
-                    legalConsultation medicalConsultation movieRecommendation musicRecommendation onlineShopping sportsRecommendation \
-                    studyConsultation therapy travelPlanning writing \
+         --topics travelPlanning \
          --n_persona ${end_persona_id} --n_samples 1 --s_persona ${start_persona_id} --s_samples 0 --output_dir data/output/ "
 
 # Print the command for debugging/logging purposes
