214bc36cde68bfbcb9b79c22be7a0d9d9df15fc4
bowen-upenn <bwjiang@seas.upenn.edu>
2025-03-23 00:52:43 -0400

    add standalone inference code for gpt series

--- scripts/inference_gpt_4o.sh
+++ scripts/inference_gpt_4o.sh
@@ -7,4 +7,4 @@ CONTEXT_PATH="data/shared_contexts_128k.jsonl"
 RESULT_PATH="data/results/eval_results_128k_${MODEL_NAME}.csv"
 
 # Run the Python script with the specified arguments
-python "inference.py" --model "$MODEL_NAME" --step "evaluate" --question_path "$QUESTION_PATH" --context_path "$CONTEXT_PATH" --result_path "$RESULT_PATH" --clean
+python "inference_standalone_openai.py" --model "$MODEL_NAME" --step "evaluate" --question_path "$QUESTION_PATH" --context_path "$CONTEXT_PATH" --result_path "$RESULT_PATH" --clean
