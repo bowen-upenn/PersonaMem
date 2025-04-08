b93a4eb800ebbbc81630a818838b96f184b293e3
bowen-upenn <bwjiang@seas.upenn.edu>
2025-04-08 01:39:33 -0400

    add more scripts

--- scripts/inference_llama4_maverick_1m.sh
+++ scripts/inference_llama4_maverick_1m.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# Limit virtual memory usage to 32GB (in kilobytes)
-ulimit -v $((32 * 1024 * 1024))  # 32 GB in KB
+# Limit virtual memory to 128GB (in KB)
+ulimit -v $((128 * 1024 * 1024))
 
 # Arguments for the Python script
-MODEL_NAME="llama3.1-8b-instruct"
-BENCHMARK_SIZE="32k"
+MODEL_NAME="llama-4-maverick-17b-128e-instruct-fp8"
+BENCHMARK_SIZE="1M"
 QUESTION_PATH="data/questions_${BENCHMARK_SIZE}.csv"
 CONTEXT_PATH="data/shared_contexts_${BENCHMARK_SIZE}.jsonl"
 RESULT_PATH="data/results/eval_results_${BENCHMARK_SIZE}_${MODEL_NAME}.csv"
+
 
 # Run the Python script with the specified arguments
 python "inference.py" --model "$MODEL_NAME" --step "evaluate" --question_path "$QUESTION_PATH" --context_path "$CONTEXT_PATH" --result_path "$RESULT_PATH" --clean
